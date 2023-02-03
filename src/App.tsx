8d5af7fc30b77c6874c9485adcec70df11960c42
olohimaiakalumhe <olohimaiakalumhe@gmail.com>
2023-02-03 16:24:54 +0000

    created functions for the form

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useMultistepForm } from "./useMultistepForm"
+
+function App() {
+  const { steps, currentStepIndex, step, isFirstStep, back, next } = useMultistepForm([
+    <div>one</div>,
+    <div>two</div>,
+  ])
+  return (
+    <div className="relative bg-white border p-4 rounded-md my-8 mx-auto max-w-xl">
+      <form action="">
+        <div className="absolute top-0 right-0 p-4">
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div className="my-2 flex justify-end gap-3">
+          {!isFirstStep && <button type="button" onClick={back} className="border py-1 px-4 rounded-md">Back</button>}
+          <button type="button" onClick={next} className="border py-1 px-4 rounded-md">Next</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default App
